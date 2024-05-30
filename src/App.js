2ec5d56f580612bb4c45cdf8c0c686af512c752f
DamVinhTrinh <150608913+DamVinhTrinh@users.noreply.github.com>
2024-05-30 23:24:27 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Func_JSX_DamVinhTrinh from './components/Func_JSX_DamVinhTrinh';
+function App() {
+  return (
+        <section className="App">
+            <h1>Demo JSX</h1>
+              {/* function Component Demo */}
+            <Func_JSX_DamVinhTrinh />
+        </section>
+  );
+}
+
+export default App;
---
+++ src/components/Func_JSX_DamVinhTrinh.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Func_JSX_DamVinhTrinh(props) {
+    const user ={
+        name:"Dam Vinh Trinh"
+        age:
+    }
+    return (
+        <div>
+            <h2>Function Component Demo</h2>
+        </div>
+    )
+}
